5cb4697f37cda1445ed0cd2ca299e68b3e71faaf
Matt Mathis <mtthwmths@gmail.com>
2015-09-29 15:02:57 -0500

    done
    done with finishing warmup

---
+++ Algorithms/Warmup/ExtraLongFactorials.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner in = new Scanner(System.in);
+
+        int num = in.nextInt();
+
+        BigInteger fact = new BigInteger(String.valueOf(num));
+
+        for (int i = 1; i < num; i++) {
+            //I don't like how this makes a new BigInteger on each iteration...
+            fact = fact.multiply(new BigInteger(String.valueOf(num - i)));
+        }
+
+        System.out.println(fact.toString());
+    }
+}
