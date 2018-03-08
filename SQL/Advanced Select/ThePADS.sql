f18bd7a44f181cb8e2d92f1556937069c83e34d3
Matt M <mtthwmths@gmail.com>
2018-03-08 14:03:52 -0600

    Create ThePADS.sql

---
+++ SQL/Advanced Select/ThePADS.sql
@@ -0,0 +1,17 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+select distinct 
+  Name||'('||substr(Occupation,1,1)||')' as OccupationShort 
+from Occupations
+Order By OccupationShort; 
+
+
+select 
+  'There are a total of '||count(Occupation) as numjobs, 
+  lower(Occupation)||'s.' as job 
+from Occupations 
+group by Occupation 
+order by numjobs, job;
