b6af96842e8a480cd482cedbef41d8446bf4dcf5
Matt M <mtthwmths@gmail.com>
2018-03-08 14:11:03 -0600

    Update ThePADS.sql
    comments and stuff

--- SQL/Advanced Select/ThePADS.sql
+++ SQL/Advanced Select/ThePADS.sql
@@ -1,6 +1,12 @@
 /*
 Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+This is for Oracle SQL.
+MySQL uses a concat function instead of '||' or '+'. This would make the first select statement look like:
+select distinct 
+  concat(Name, '(', substr(Occupation,1,1), ')') as OccupationShort 
+from Occupations 
+Order By OccupationShort;
 */
 
 select distinct 
